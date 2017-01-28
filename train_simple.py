18acd206da2329904c476fa29d62d2126367e230
Bryan Hampton <bryan@zeropointfrontiers.com>
2017-01-28 15:30:15 -0600

    Cleanup of the training script to get ready for better NN model.

--- train_simple.py
+++ train_simple.py
@@ -1,25 +1,39 @@
-
 import numpy as np
 from preprocess import *
 from sklearn.utils import shuffle
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Lambda, ELU, MaxPooling2D, Convolution2D
 
+LOG_FILE = 'simple_train.csv'
+MODEL_NAME = 'model_s'
+N_EPOCHS = 5
+BATCH_SIZE=30
 
-
+# pre-process data:
 N_ROWS = 160-80
 N_COLS = 320
 IMG_SHAPE = (N_ROWS, N_COLS, 3)
 
-def simpleModel():
+def driveModel():
+    """Super simple NN model to test process flow"""
+    
     model = Sequential()
+    # Normalize each pixel
     model.add(Lambda(lambda x: x/127.5 - 1., input_shape=IMG_SHAPE, output_shape=IMG_SHAPE))
+    
+    # 1st Convolution
     model.add(Convolution2D(24, 3, 3, subsample=(2,2), name="Conv2D_1", input_shape=IMG_SHAPE))
     model.add(ELU())
+    
+    # Flatten for connected layers
     model.add(Flatten(name="Flatten"))
+    
+    # Fully connect to output
     model.add(Dense(1, name="Output"))
+    
     model.compile(loss="mse", optimizer="adam")
     return model
+
 
 def generateBatch(logdata, batch_size=128):
     while logdata:
@@ -40,26 +54,25 @@ def generateBatch(logdata, batch_size=128):
                 batch_y = []
 
 
-INPUT_SHAPE = [160, 320, 3]
-SAMPLES_PER_EPOCH = 150
-VALID_SAMPLES_PER_EPOCH = 50
-EPOCHS = 5
-
 if __name__ == '__main__':
-    model = simpleModel()
+    
+    logdata = LogData(filename = LOG_FILE)
+    nSamplesTotal = len(logdata.rows)
+    nSamplesValid = nSamplesTotal * 0.2
+    
+    model = driveModel()
     model.summary()
-    #plot(model, to_file="model.png", show_shapes=True)
     
-    logdata = LogData('simple_train.csv')
+    generatorTrain = generateBatch(logdata, BATCH_SIZE)
+    generatorValid = generateBatch(logdata, BATCH_SIZE)
     
-    generatorTrain = generateBatch(logdata, 30)
-    generatorValid = generateBatch(logdata, 30)
+    history = model.fit_generator(
+                generator = generatorTrain,
+                samples_per_epoch = nSamplesTotal,
+                nb_epoch = N_EPOCHS,
+                verbose = 2,
+                validation_data = generatorValid,
+                nb_val_samples  = nSamplesValid  )
     
-    history = model.fit_generator( generatorTrain,
-                SAMPLES_PER_EPOCH,
-                EPOCHS,
-                validation_data=generatorValid, verbose=2,
-                nb_val_samples=VALID_SAMPLES_PER_EPOCH)
-    
-    model.save_weights("model_s.h5")
-    open("model_s.json", "w").write(model.to_json(indent=4))
+    model.save_weights(MODEL_NAME+".h5")
+    open(MODEL_NAME+".json", "w").write(model.to_json(indent=4))

10bc42d4f600c746c490354fb7996b226ad699e9
Bryan Hampton <bryan@zeropointfrontiers.com>
2017-01-29 11:03:34 -0600

    Adding camera choice to batch generation.  Also adding image flip when there is a non-zero steering angle.

--- train_simple.py
+++ train_simple.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 from preprocess import *
 from sklearn.utils import shuffle
 from keras.models import Sequential
@@ -7,7 +8,9 @@ from keras.layers import Dense, Dropout, Flatten, Lambda, ELU, MaxPooling2D, Con
 LOG_FILE = 'simple_train.csv'
 MODEL_NAME = 'model_s'
 N_EPOCHS = 5
-BATCH_SIZE=30
+BATCH_SIZE=25
+N_SAMP_TRAIN = 400
+N_SAMP_VALID = N_SAMP_TRAIN * 0.2
 
 # pre-process data:
 N_ROWS = 160-80
@@ -40,25 +43,45 @@ def generateBatch(logdata, batch_size=128):
         batch_x = []
         batch_y = []
         for row in logdata.rows:
-            image = mpimg.imread(os.path.join(DATA_DIR, row['center']))
-            image = image[60:140, 0:320]  # crop top and bottom
-            steering = float(row['steering'])
+            # there are so many images with no steering angle
+            # so we will skip 1/2 of them
+            if random.choice(['Skip', 'Keep']) == 'Skip':
+                continue
             
+            # we will choose between the 3 cameras and get the image
+            camera = random.choice(['left','center','right'])
+            image  = mpimg.imread(os.path.join(DATA_DIR, row[camera].strip()))
+            image  = image[60:140, 0:320]  # crop top and bottom
+            
+            steering = float(row['steering'])
+            #adjust steering for left/right camera angles
+            if camera == 'left' : steering += 0.2
+            if camera == 'right': steering -= 0.2
+                        
             batch_x.append(np.reshape(image, (1, N_ROWS, N_COLS, 3)))
             batch_y.append(np.array([[steering]]))
             
+            # to augment data with left/right steering we will flip images
+            # that have non-zero steering which will double those samples
+            if steering < -0.01 or steering > 0.01 and len(batch_x) < batch_size:
+                image = cv2.flip(image, 1)
+                steering *= -1
+                batch_x.append(np.reshape(image, (1, N_ROWS, N_COLS, 3)))
+                batch_y.append(np.array([[steering]]))
+            
+            # yield an entire batch at once
             if len(batch_x) == batch_size:
                 batch_x, batch_y, = shuffle(batch_x, batch_y, random_state=21)
                 yield (np.vstack(batch_x), np.vstack(batch_y))
                 batch_x = []
                 batch_y = []
+            
+
 
 
 if __name__ == '__main__':
     
     logdata = LogData(filename = LOG_FILE)
-    nSamplesTotal = len(logdata.rows)
-    nSamplesValid = nSamplesTotal * 0.2
     
     model = driveModel()
     model.summary()
@@ -68,11 +91,11 @@ if __name__ == '__main__':
     
     history = model.fit_generator(
                 generator = generatorTrain,
-                samples_per_epoch = nSamplesTotal,
+                samples_per_epoch = N_SAMP_TRAIN,
                 nb_epoch = N_EPOCHS,
                 verbose = 2,
                 validation_data = generatorValid,
-                nb_val_samples  = nSamplesValid  )
+                nb_val_samples  = N_SAMP_VALID  )
     
     model.save_weights(MODEL_NAME+".h5")
     open(MODEL_NAME+".json", "w").write(model.to_json(indent=4))
